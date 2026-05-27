d300ebc248f30ed545465f6377822dd7474dfdcf
AlperenSagutdev <alperensagut5569@gmail.com>
2026-05-27 17:02:58 +0300

    Server and Client updated
    Server and Client updated

--- udp-client.c
+++ udp-client.c
@@ -1,124 +1,145 @@
 #include "contiki.h"
 #include "net/routing/routing.h"
-#include "random.h"
 #include "net/netstack.h"
 #include "net/ipv6/simple-udp.h"
-#include "ota-metadata.h"
+#include "cfs/cfs.h"
 #include <stdint.h>
-#include <inttypes.h>
 #include <string.h>
+#include <stdbool.h>
 
 #include "sys/node-id.h"
 #include "sys/log.h"
 #define LOG_MODULE "App"
 #define LOG_LEVEL LOG_LEVEL_INFO
 
-#define WITH_SERVER_REPLY  1
-#define UDP_CLIENT_PORT	8765
-#define UDP_SERVER_PORT	5678
+#define UDP_CLIENT_PORT 8765
+#define UDP_SERVER_PORT 5678
+#define BLOCK_SIZE 64
+#define TOTAL_BLOCKS 2080 
 
-#define SEND_INTERVAL		  (10 * CLOCK_SECOND)
-
-static struct simple_udp_connection udp_conn;
-static uint32_t rx_count = 0;
-static ota_boot_metadata_t boot_metadata = {
-  .magic = OTA_IMAGE_MAGIC,
-  .active_slot = OTA_SLOT_A,
-  .candidate_slot = OTA_SLOT_NONE,
-  .state_a = OTA_IMAGE_STATE_CONFIRMED,
-  .state_b = OTA_IMAGE_STATE_EMPTY,
+struct __attribute__((packed)) firmware_packet {
+    uint16_t block_num;          
+    uint8_t data_len;            
+    uint16_t checksum;           
+    uint8_t data[BLOCK_SIZE];    
 };
 
-/*---------------------------------------------------------------------------*/
+struct __attribute__((packed)) ack_packet {
+    uint16_t block_num;          
+    uint8_t status;              
+};
+
+static struct simple_udp_connection udp_conn;
+static struct firmware_packet current_pkt;
+static uint16_t expected_ack = 0;
+static bool file_transfer_complete = false;
+static int file_fd = -1;
+
+uint16_t calculate_checksum(const uint8_t *data, uint8_t len) {
+    uint16_t checksum = 0;
+    for(uint8_t i = 0; i < len; i++) {
+        checksum ^= data[i];
+    }
+    return checksum;
+}
+
 PROCESS(udp_client_process, "UDP client");
 AUTOSTART_PROCESSES(&udp_client_process);
-/*---------------------------------------------------------------------------*/
+
 static void
 udp_rx_callback(struct simple_udp_connection *c,
-         const uip_ipaddr_t *sender_addr,
-         uint16_t sender_port,
-         const uip_ipaddr_t *receiver_addr,
-         uint16_t receiver_port,
-         const uint8_t *data,
-         uint16_t datalen)
+         const uip_ipaddr_t *sender_addr, uint16_t sender_port,
+         const uip_ipaddr_t *receiver_addr, uint16_t receiver_port,
+         const uint8_t *data, uint16_t datalen)
 {
-  static uint32_t fake_version = 2;
-  uint32_t fake_image_crc;
 
-  (void)c;
-  (void)sender_port;
-  (void)receiver_addr;
-  (void)receiver_port;
+  if(node_id != 2) {
+    return; 
+  }
 
-  LOG_INFO("Client received response '%.*s' from ", datalen, (char *) data);
-  LOG_INFO_6ADDR(sender_addr);
-#if LLSEC802154_CONF_ENABLED
-  LOG_INFO_(" LLSEC LV:%d", uipbuf_get_attr(UIPBUF_ATTR_LLSEC_LEVEL));
-#endif
-  LOG_INFO_("\n");
-  rx_count++;
+  if(sender_addr->u8[15] != 1) {
+    LOG_INFO("Uyari: Yabanci bir dugumden paket alindi, yok sayiliyor.\n");
+    return;
+  }
+  // ---------------------------
 
-  /*
-   * Placeholder integration for the first OTA path.
-   * In the real receiver, this information must come from the fully assembled
-   * Slot B image stored in flash.
-   */
-  fake_image_crc = ota_crc32_buffer(data, datalen);
-  if(ota_metadata_mark_verified(&boot_metadata, OTA_SLOT_B,
-                                fake_version, datalen, fake_image_crc) &&
-     ota_metadata_stage_verified_image(&boot_metadata, OTA_SLOT_B)) {
-    LOG_INFO("OTA metadata updated: slot B staged for activation\n");
+  if(datalen == sizeof(struct ack_packet)) {
+    struct ack_packet *ack = (struct ack_packet *)data;
+    if(ack->status == 1 && ack->block_num == expected_ack) {
+      LOG_INFO("ACK alindi: Blok %u\n", ack->block_num);
+      expected_ack++;
+      process_poll(&udp_client_process);
+    }
   }
 }
-/*---------------------------------------------------------------------------*/
+
 PROCESS_THREAD(udp_client_process, ev, data)
 {
-  static struct etimer periodic_timer;
-  static char str[32];
+  static struct etimer timeout_timer;
   uip_ipaddr_t dest_ipaddr;
-  static uint32_t tx_count;
-  static uint32_t missed_tx_count;
+  static uint16_t target_ack = 0; 
 
   PROCESS_BEGIN();
 
-  /* Initialize UDP connection */
-  simple_udp_register(&udp_conn, UDP_CLIENT_PORT, NULL,
-                      UDP_SERVER_PORT, udp_rx_callback);
+  simple_udp_register(&udp_conn, UDP_CLIENT_PORT, NULL, UDP_SERVER_PORT, udp_rx_callback);
 
-  etimer_set(&periodic_timer, random_rand() % SEND_INTERVAL);
-  while(1) {
-    PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&periodic_timer));
-
-    if(NETSTACK_ROUTING.node_is_reachable() &&
-        NETSTACK_ROUTING.get_root_ipaddr(&dest_ipaddr)) {
-
-// Bu blok 3 numarali cihazda calismaz. 2-den-1-e gonderim icin yapildi. 3 numarali
-// cihaz komsuluk gorevi yapar. İletime yardim eder.
-
-      if(node_id == 2) {
-
-        LOG_INFO("Sending request %" PRIu32 " to ", tx_count);
-        LOG_INFO_6ADDR(&dest_ipaddr);
-        LOG_INFO_("\n");
-
-        snprintf(str, sizeof(str), "Merhaba %" PRIu32, tx_count);
-        simple_udp_sendto(&udp_conn, str, strlen(str), &dest_ipaddr);
-        tx_count++;
+  if(node_id == 2) {
+    int check_fd = cfs_open("new-firmware.z1", CFS_READ);
+    if(check_fd < 0) {
+      LOG_INFO("Disk bos! 130 KB boyutunda sanal 'new-firmware.z1' olusturuluyor, lutfen bekleyin...\n");
+      int write_fd = cfs_open("new-firmware.z1", CFS_WRITE);
+      if(write_fd >= 0) {
+        uint8_t dummy_data[BLOCK_SIZE];
+        for(int i = 0; i < BLOCK_SIZE; i++) dummy_data[i] = i; 
+        
+        for(int i = 0; i < TOTAL_BLOCKS; i++) {
+          cfs_write(write_fd, dummy_data, BLOCK_SIZE);
+        }
+        cfs_close(write_fd);
+        LOG_INFO("130 KB dosya CFS diske yazildi! Aktarim hazir.\n");
       }
-
-
     } else {
-      LOG_INFO("Not reachable yet\n");
-      if(tx_count > 0) {
-        missed_tx_count++;
-      }
+      cfs_close(check_fd);
     }
+  }
 
-    /* Add some jitter */
-    etimer_set(&periodic_timer, SEND_INTERVAL
-      - CLOCK_SECOND + (random_rand() % (2 * CLOCK_SECOND)));
+  file_fd = cfs_open("new-firmware.z1", CFS_READ);
+
+  while(!file_transfer_complete && file_fd >= 0) {
+    if(NETSTACK_ROUTING.node_is_reachable() && NETSTACK_ROUTING.get_root_ipaddr(&dest_ipaddr)) {
+      if(node_id == 2) {
+        
+        // SİHİRLİ DÜZELTME: (uint32_t) ile çarpım taşması engellendi
+        cfs_seek(file_fd, (uint32_t)expected_ack * BLOCK_SIZE, CFS_SEEK_SET);
+        int read_bytes = cfs_read(file_fd, current_pkt.data, BLOCK_SIZE);
+        
+        if(read_bytes <= 0) {
+          LOG_INFO("Aktarim bitti! Dosya basariyla gonderildi.\n");
+          file_transfer_complete = true;
+          cfs_close(file_fd);
+          break;
+        }
+
+        current_pkt.block_num = expected_ack;
+        current_pkt.data_len = read_bytes;
+        current_pkt.checksum = calculate_checksum(current_pkt.data, read_bytes);
+
+        target_ack = expected_ack; 
+
+        while(expected_ack == target_ack) {
+          LOG_INFO("Blok %u gonderiliyor (%d bayt)...\n", current_pkt.block_num, current_pkt.data_len);
+          simple_udp_sendto(&udp_conn, &current_pkt, sizeof(struct firmware_packet), &dest_ipaddr);
+
+          etimer_set(&timeout_timer, CLOCK_SECOND * 1);
+          PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timeout_timer) || ev == PROCESS_EVENT_POLL);
+        }
+      }
+    } else {
+      LOG_INFO("Root henuz erisilebilir degil...\n");
+      etimer_set(&timeout_timer, CLOCK_SECOND * 2);
+      PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timeout_timer));
+    }
   }
 
   PROCESS_END();
 }
-/*---------------------------------------------------------------------------*/
--- udp-server.c
+++ udp-server.c
@@ -31,7 +31,7 @@
 #include "net/routing/routing.h"
 #include "net/netstack.h"
 #include "net/ipv6/simple-udp.h"
-
+#include "cfs/cfs.h"
 #include "sys/log.h"
 #define LOG_MODULE "App"
 #define LOG_LEVEL LOG_LEVEL_INFO
@@ -39,7 +39,30 @@
 #define WITH_SERVER_REPLY  1
 #define UDP_CLIENT_PORT	8765
 #define UDP_SERVER_PORT	5678
+#define BLOCK_SIZE 64
 
+struct __attribute__((packed)) firmware_packet {
+    uint16_t block_num;          // Sıralama (Sequence) numarası
+    uint8_t data_len;            // Blok içindeki gerçek veri boyutu (Son blok < 64 olabilir)
+    uint16_t checksum;           // Parça doğrulama için checksum
+    uint8_t data[BLOCK_SIZE];    // 64 baytlık makine kodu (binary veri)
+};
+
+struct __attribute__((packed)) ack_packet {
+    uint16_t block_num;          // Alınan/Onaylanan blok numarası
+    uint8_t status;              // 1: ACK (Başarılı), 0: NACK (Hatalı parça)
+};
+
+uint16_t calculate_checksum(const uint8_t *data, uint8_t len) {
+    uint16_t checksum = 0;
+    for(uint8_t i = 0; i < len; i++) {
+        checksum ^= data[i];
+    }
+    return checksum;
+}
+
+static int out_fd = -1;
+static uint16_t next_expected_block = 0;
 static struct simple_udp_connection udp_conn;
 
 PROCESS(udp_server_process, "UDP server");
@@ -47,33 +70,58 @@ AUTOSTART_PROCESSES(&udp_server_process);
 /*---------------------------------------------------------------------------*/
 static void
 udp_rx_callback(struct simple_udp_connection *c,
-         const uip_ipaddr_t *sender_addr,
-         uint16_t sender_port,
-         const uip_ipaddr_t *receiver_addr,
-         uint16_t receiver_port,
-         const uint8_t *data,
-         uint16_t datalen)
+         const uip_ipaddr_t *sender_addr, uint16_t sender_port,
+         const uip_ipaddr_t *receiver_addr, uint16_t receiver_port,
+         const uint8_t *data, uint16_t datalen)
 {
-  LOG_INFO("Received request '%.*s' from ", datalen, (char *) data);
-  LOG_INFO_6ADDR(sender_addr);
-  LOG_INFO_("\n");
-#if WITH_SERVER_REPLY
-  /* send back the same string to the client as an echo reply */
-  LOG_INFO("Sending response.\n");
-  simple_udp_sendto(&udp_conn, data, datalen, sender_addr);
-#endif /* WITH_SERVER_REPLY */
+  struct ack_packet response;
+
+  if(datalen == sizeof(struct firmware_packet)) {
+    struct firmware_packet *pkt = (struct firmware_packet *)data;
+    
+    uint16_t calc_chk = calculate_checksum(pkt->data, pkt->data_len);
+    if(calc_chk != pkt->checksum) {
+      LOG_ERR("Blok %u bozuk! (Checksum hatasi)\n", pkt->block_num);
+      response.block_num = pkt->block_num;
+      response.status = 0; // NACK
+      simple_udp_sendto(&udp_conn, &response, sizeof(struct ack_packet), sender_addr);
+      return;
+    }
+
+    if(pkt->block_num == next_expected_block) {
+      // Doğru blok geldiyse diske yaz
+      if(out_fd >= 0) {
+        cfs_write(out_fd, pkt->data, pkt->data_len);
+        LOG_INFO("Blok %u diske yazildi (%d bayt).\n", pkt->block_num, pkt->data_len);
+      }
+      next_expected_block++;
+    } else if (pkt->block_num < next_expected_block) {
+      LOG_INFO("Eski/Mükerrer blok %u alindi, tekrar ACK yollaniyor.\n", pkt->block_num);
+    } else {
+      LOG_WARN("Beklenmeyen blok %u geldi. Beklenen: %u\n", pkt->block_num, next_expected_block);
+      return; 
+    }
+
+    response.block_num = pkt->block_num;
+    response.status = 1; // ACK
+    simple_udp_sendto(&udp_conn, &response, sizeof(struct ack_packet), sender_addr);
+  }
 }
 /*---------------------------------------------------------------------------*/
 PROCESS_THREAD(udp_server_process, ev, data)
 {
   PROCESS_BEGIN();
 
-  /* Initialize DAG root */
   NETSTACK_ROUTING.root_start();
 
-  /* Initialize UDP connection */
+  cfs_remove("received.elf");
+  out_fd = cfs_open("received.elf", CFS_WRITE );
+  if(out_fd < 0) {
+    LOG_ERR("HATA: received.elf olusturulamadi!\n");
+  }
+
   simple_udp_register(&udp_conn, UDP_SERVER_PORT, NULL,
-                      UDP_CLIENT_PORT, udp_rx_callback);
+                        UDP_CLIENT_PORT, udp_rx_callback);
 
   PROCESS_END();
 }
